744b32b4f055e618c7efd5effb015c8f08c192b0
Tiejun Gao <2033555+tjgao@users.noreply.github.com>
2026-09-12 22:37:43 +1000

    UI and format adjustment for yazi confirm-quit

--- yazi/.config/yazi/plugins/confirm-quit.yazi/main.lua
+++ yazi/.config/yazi/plugins/confirm-quit.yazi/main.lua
@@ -1,13 +1,13 @@
 local function entry()
-  local yes = ya.confirm({
-    pos = { "center", w = 56, h = 9 },
-    title = "Quit Yazi?",
-    body = ui.Text("Exit Yazi now?"):wrap(ui.Wrap.YES),
-  })
+    local yes = ya.confirm({
+        pos = { "center", w = 56, h = 9 },
+        title = "",
+        body = ui.Text("\n\nExit Yazi now?"):wrap(ui.Wrap.YES),
+    })
 
-  if yes then
-    ya.emit("quit", {})
-  end
+    if yes then
+        ya.emit("quit", {})
+    end
 end
 
 return { entry = entry }
